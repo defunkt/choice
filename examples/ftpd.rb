a1f2d0a9e9bdb41c69d17af82852b39e2576127a
Chris Wanstrath <chris@ozmm.org>
2006-04-29 07:51:18 +0000

    adding ftpd.rb example file
    git-svn-id: http://choice.rubyforge.org/svn/trunk@5 2891bcfe-6912-0410-8cba-feaf504d3ce4

---
+++ examples/ftpd.rb
@@ -0,0 +1,78 @@
+$:.unshift "../lib"
+require 'choice'
+
+port = 21
+
+Choice.options do
+  #banner "Usage: ftpd.rb [options]"
+  
+  header ""
+  header "Specific options:" 
+  
+  option :host do
+    short '-h'
+    long '--host=HOST'
+    desc "The hostname or ip of the host to bind to (default 127.0.0.1)"
+    default '127.0.0.1'
+  end
+
+  option :port do
+    short '-p'
+    long '--port=PORT'
+    desc "The port to listen on (default 21)"
+    cast Integer
+    default port
+  end
+
+  option :clients do
+    short '-c'
+    long '--clients=NUM'
+    cast Integer
+    desc "The number of connections to allow at once (default 5)"
+    default 5
+  end
+      
+  option :yaml_cfg do
+    long '--config=FILE'
+    desc 'Load configuration from YAML file'
+  end
+  
+  option :sample do
+    long '--sample'
+    desc "See a sample YAML config file"
+    action do
+      puts "See!"
+      exit
+    end
+  end
+  
+  option :debug do
+    short '-d'
+    long '--debug'
+    desc 'Turn on debugging mode'
+  end  
+  
+   separator ''
+   separator 'Common options: '
+
+  option :help do
+    long '--help'
+    desc 'Show this message'
+  end
+
+  option :version do
+    short '-v'
+    long '--version'
+    desc 'Show version'
+    action do
+      puts "ftpd.rb FTP server v4"
+      exit      
+    end
+  end
+  
+  
+     
+end
+
+print "Choices: "
+puts Choice.choices.inspect
