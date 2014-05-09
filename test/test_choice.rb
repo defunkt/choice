37b5f0c26abae1b1feb4dad03e6505e4416ecf72
Tyler Dooling <tdooling@decisiv.net>
2014-05-09 17:01:20 -0500

    Fix broken tests.

--- test/test_choice.rb
+++ test/test_choice.rb
@@ -5,13 +5,13 @@ require 'choice'
 $VERBOSE = nil
 
 class TestChoice < Test::Unit::TestCase
-  
+
   def setup
     Choice.reset!
     Choice.dont_exit_on_help = true
     Choice.send(:class_variable_set, '@@choices', {})
   end
-    
+
   def test_choices
     Choice.options do
       header "Tell me about yourself?"
@@ -63,10 +63,10 @@ class TestChoice < Test::Unit::TestCase
         short '-m'
         desc 'Your favorite meal.'
       end
-      
+
       separator ""
       separator "And you eat it with..."
-      
+
       option :utencil do
         short "-u"
         long "--utencil[=UTENCIL]"
@@ -75,23 +75,23 @@ class TestChoice < Test::Unit::TestCase
     end
 
     Choice.args = ['-m', 'lunch', '--help']
-    
+
     help_string = <<-HELP
 Usage: choice [-mu]
 
     -m                               Your favorite meal.
-
-And you eat it with...
+                                     
+                                     
     -u, --utencil[=UTENCIL]          Your favorite eating utencil.
-HELP
+    HELP
 
     assert_equal help_string, HELP_STRING
   end
-  
+
   UNKNOWN_STRING = ''
   def test_unknown_argument
     Choice.output_to(UNKNOWN_STRING)
-    
+
     Choice.options do
       banner "Usage: choice [-mu]"
       header ""
@@ -99,10 +99,10 @@ HELP
         short '-m'
         desc 'Your favorite meal.'
       end
-      
+
       separator ""
       separator "And you eat it with..."
-      
+
       option :utencil do
         short "-u"
         long "--utencil[=UTENCIL]"
@@ -111,23 +111,23 @@ HELP
     end
 
     Choice.args = ['-m', 'lunch', '--motorcycles']
-    
+
     help_string = <<-HELP
 Usage: choice [-mu]
 
     -m                               Your favorite meal.
-
-And you eat it with...
+                                     
+                                     
     -u, --utencil[=UTENCIL]          Your favorite eating utencil.
-HELP
-    
+    HELP
+
     assert_equal help_string, UNKNOWN_STRING
   end
-  
+
   REQUIRED_STRING = ''
   def test_required_argument
     Choice.output_to(REQUIRED_STRING)
-    
+
     Choice.options do
       banner "Usage: choice [-mu]"
       header ""
@@ -135,10 +135,10 @@ HELP
         short '-m'
         desc 'Your favorite meal.'
       end
-      
+
       separator ""
       separator "And you eat it with..."
-      
+
       option :utencil do
         short "-u"
         long "--utencil[=UTENCIL]"
@@ -147,16 +147,16 @@ HELP
     end
 
     Choice.args = ['-u', 'spork']
-    
+
     help_string = <<-HELP
 Usage: choice [-mu]
 
     -m                               Your favorite meal.
-
-And you eat it with...
+                                     
+                                     
     -u, --utencil[=UTENCIL]          Your favorite eating utencil.
-HELP
-    
+    HELP
+
     assert_equal help_string, REQUIRED_STRING
   end
 
@@ -165,25 +165,25 @@ HELP
       header "Tell me about yourself?"
       header ""
       options :band => { :short => "-b", :long => "--band=BAND", :cast => String, :desc => ["Your favorite band.", "Something cool."],
-                        :validate => /\w+/ },
-              :animal => { :short => "-a", :long => "--animal=ANIMAL", :cast => String, :desc => "Your favorite animal." }
-      
+                         :validate => /\w+/ },
+                         :animal => { :short => "-a", :long => "--animal=ANIMAL", :cast => String, :desc => "Your favorite animal." }
+
       footer ""
       footer "--help This message"
     end
-    
+
     band = 'LedZeppelin'
     animal = 'Reindeer'
-    
+
     args = ['-b', band, "--animal=#{animal}"]
     Choice.args = args
-    
+
     assert_equal band, Choice.choices['band']
     assert_equal animal, Choice.choices[:animal]
     assert_equal ["Tell me about yourself?", ""], Choice.header
     assert_equal ["", "--help This message"], Choice.footer
   end
-  
+
   def test_args_of
     suits = %w[clubs diamonds spades hearts]
     stringed_numerics = (1..13).to_a.map { |a| a.to_s }
@@ -191,7 +191,7 @@ HELP
     cards = {}
     stringed_numerics.each { |n| cards[n] = n }
     cards.merge!('1' => 'ace', '11' => 'jack', '12' => 'queen', '13' => 'king')
-    
+
     Choice.options do
       header "Gambling is fun again!  Pick a card and a suit (or two), then see if you win!"
       header ""
@@ -215,7 +215,7 @@ HELP
         valid valid_cards
         cast String
       end
-      
+
       #cheat! to test --option=
       option :autowin do
         short '-a'
@@ -224,7 +224,7 @@ HELP
         desc 'Beware: raises the suspitions of other players'
       end
     end
-    
+
     args = ["-c", "king", "--suit", "clubs", "diamonds", "spades", "hearts", "--autowin", "Grant"]
     Choice.args = args
     assert_equal ["king"], Choice.args_of("-c")
