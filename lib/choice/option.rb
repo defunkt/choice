7bb3d0a537d409b83f38ff4891fa68fd24188fff
Grant Austin <gaustin@gmail.com>
2014-12-31 23:36:32 -0700

    Change hash to instance variables

--- lib/choice/option.rb
+++ lib/choice/option.rb
@@ -6,17 +6,15 @@ module Choice
     # You can instantiate an option on its own or by passing it a name and
     # a block.  If you give it a block, it will eval() the block and set itself
     # up nicely.
-
     def initialize(required = false, &block)
-      @choices = {}
-
       # If we got a block, eval it and set everything up.
       instance_eval(&block) if block_given?
 
       # Is this option required?
-
-      @choices['required'] = required
+      @required = required
     end
+
+    attr_reader :required
 
     def method_missing(method, *args, &block)
       # Mask NoMethodError
@@ -24,99 +22,89 @@ module Choice
       raise ParseError, "I don't know `#{method}'"
     end
 
-    def required
-      @choices['required']
-    end
-
     def short(value=nil)
-      value_setter('short', value)
-      @choices['short']
+      @short ||= value
     end
 
     def short?
-      @choices['short']
+      @short
     end
 
     def long(value=nil)
-      value_setter('long', value)
-      @choices['long']
+      @long ||= value
     end
 
     def long?
-      @choices['long']
+      @long
     end
 
     def default(value=nil)
-      value_setter('default', value)
-      @choices['default']
+      @default ||= value
     end
 
     def default?
-      @choices['default']
+      @default
     end
 
     def cast(value=nil)
-      value_setter('cast', value)
-      @choices['cast']
+      @cast ||= value
     end
 
     def cast?
-      @choices['cast']
+      @cast
     end
 
     def valid(value=nil)
-      value_setter('valid', value)
-      @choices['valid']
+      @valid ||= value
     end
 
     def valid?
-      @choices['valid']
+      @valid
     end
 
     # TODO: Should this be split into two different validate methods?
     def validate(value=nil, &block)
-      if !value.nil?
-        value_setter('validate', value)
-      elsif !block.nil?
-        block_setter('validate', &block)
-      end
-      @choices['validate']
+      @validate ||= if !value.nil?
+                      value
+                    elsif !block.nil?
+                      block
+                    end
     end
 
     def validate?
-      @choices['validate']
+      @validate
     end
 
     def action(&block)
-      block_setter('action', &block)
-      @choices['action']
+      @action ||= block
     end
 
     def action?
-      @choices['action']
+      @action
     end
 
     def filter(&block)
-      block_setter('filter', &block)
-      @choices['filter']
+      @filter ||= block
     end
 
     def filter?
-      @choices['filter']
+      @filter
     end
 
     # The desc method is slightly special: it stores itself as an array and
     # each subsequent call adds to that array, rather than overwriting it.
     # This is so we can do multi-line descriptions easily.
     def desc(string = nil)
-      return @choices['desc'] if string.nil?
+      return @desc if string.nil?
 
-      @choices['desc'] ||= []
-      @choices['desc'].push(string)
+      @desc ||= []
+      @desc.push(string)
     end
 
     # Simple, desc question method.
-    def desc?() !!@choices['desc'] end
+    def desc?
+      @desc
+    end
 
     # Returns Option converted to an array.
     def to_a
@@ -136,21 +124,18 @@ module Choice
 
     # Returns Option converted to a hash.
     def to_h
-      @choices.dup
-    end
-
-    private
-
-    def value_setter(name, value)
-      if !value.nil?
-        @choices[name] = value
-      end
-    end
-
-    def block_setter(name, &block)
-      if block
-        @choices[name] = block
-      end
+      {
+        "required" => required,
+        "short" => short,
+        "long" => long,
+        "desc" => desc,
+        "default" => default,
+        "filter" => filter,
+        "action" => action,
+        "cast" => cast,
+        "valid" => valid,
+        "validate" => validate
+      }.reject {|k, v| v.nil? }
     end
 
     # In case someone tries to use a method we don't know about in their
